153469de6bc8b891b5d0721e8c5291f5bf126e61
Fadi Atef <155027038+FadiAtef@users.noreply.github.com>
2025-10-24 16:56:09 +0300

    + create login screen

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -1,16 +1,23 @@
 import 'package:egytravel_app/core/routes/app_routes.dart';
+import 'package:egytravel_app/feature/authentacition/logic/binding/login_binding.dart';
+import 'package:egytravel_app/feature/authentacition/presentation/screens/login_view.dart';
 import 'package:egytravel_app/feature/splash/logic/binding/splash_binding.dart';
 import 'package:egytravel_app/feature/splash/ui/screen/splash_screen.dart';
 import 'package:get/get.dart';
 
 class AppPages {
-  static const initial = Routes.splashScreen;
+  static const initial = Routes.loginView;
 
   static List<GetPage> appPages() => [
     GetPage(
       name: Routes.splashScreen,
       page: () => const SplashScreen(),
       binding: SplashBinding(),
+    ),
+    GetPage(
+      name: Routes.loginView,
+      page: () => const LoginScreen(),
+      binding: LoginBinding(),
     ),
   ];
 }
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,4 +1,5 @@
 class Routes {
   static const String splashScreen = '/splash_screen';
+  static const String loginView = '/login_view';
   static const String home = '/home';
 }
