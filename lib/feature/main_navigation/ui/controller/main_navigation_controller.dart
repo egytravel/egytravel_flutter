02a834ff29c1f73663af7482d0be806348dca40f
younangamal <younangamal722.@gmail>
2025-11-22 14:04:45 +0200

    edit in home

--- lib/feature/main_navigation/ui/controller/main_navigation_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class MainNavigationController extends GetxController {
-  var currentIndex = 0.obs;
-
-  void changeTab(int index) {
-    currentIndex.value = index;
-  }
-}
