1ac5c85ec95ae7403cc950a969ca61b66e3c60a6
younangamal <younangamal722.@gmail>
2026-04-18 11:07:09 +0200

    feat: add skeletonizer dependency and define API endpoint constants

--- lib/core/network/end_point.dart
+++ lib/core/network/end_point.dart
@@ -1,6 +1,9 @@
 class EndPoint {
-  static const baseUrl='https://egytravelbackend-production.up.railway.app/api';
-  static const login='/auth/login';
+  static const baseUrl='https://egy-travel-89eca3b6683d.herokuapp.com';
+  static const login='/api/auth/';
   static const register='/auth/register';
-
+  static const home ='/api/home/';
+  static const explore ='/api/explore/';
+  static const booking ='/api/booking/';
+  static const tripPlanner ='/api/tripPlanner/';
 }
